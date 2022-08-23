8339d3926dae4db82c5d1c4ada8f4f4afa2fb5c6
chirantan2003 <chirantan2003@gmail.com>
2022-08-23 20:28:39 +0530

    feat: sponsors

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Speakers from "../components/speakers/speakers";
 import Navbar from "../components/widgets/navbar/navbar";
 import styles from "../styles/Home.module.css";
 import Hero from "../components/hero/hero";
+import Sponsors from "../components/sponsors/sponsors";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <div className={styles.container}>
         <Hero />
         <Speakers />
+        <br></br>
+        <Sponsors />
       </div>
       <Footer />
     </>
