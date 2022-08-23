95d14d0404be9ba0301857ca6ff0204df9e4bc68
chirantan2003 <chirantan2003@gmail.com>
2022-08-23 20:18:26 +0530

    feat: hero page

---
+++ components/core/button/button.js
@@ -0,0 +1,21 @@
+import styles from "./Button.module.css";
+import Image from "Next/image";
+
+const Button = () => {
+  return (
+    <button className={`${styles.button} ${styles.noselect}`}>
+      <span className={styles.text}>Register Now</span>
+      <span className={styles.icon}>
+        <Image
+          src={"/arrow.svg"}
+          width="80"
+          height="60"
+          viewBox="0 0 24 24"
+          alt="arrow"
+        />
+      </span>
+    </button>
+  );
+};
+
+export default Button;
