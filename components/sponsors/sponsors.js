2fcb7fb593d13c5d44ac916e61a8f072518dce9e
chirantan2003 <chirantan2003@gmail.com>
2022-08-23 20:28:39 +0530

    feat: sponsors

--- components/sponsors/sponsors.js
+++ components/sponsors/sponsors.js
@@ -3,8 +3,20 @@ import Image from "Next/image";
 
 const Sponsors = () => {
   return (
-    <div>
-      <div></div>
+    <div className="darkBg">
+      <h1>Sponsors</h1>
+      <div className={styles.outer}>
+        <div className={styles.img}>
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+          <Image src={"/googleSp.svg"} height="200" width="200" alt="sponsor" />
+        </div>
+      </div>
     </div>
   );
 };
