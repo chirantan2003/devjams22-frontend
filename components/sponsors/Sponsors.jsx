eafc564c206c60c7c1d1ac5dfb1e87c6ff827005
chirantan2003 <chirantan2003@gmail.com>
2022-08-26 01:21:06 +0530

    style: reduce sponsors

--- components/sponsors/Sponsors.jsx
+++ components/sponsors/Sponsors.jsx
@@ -17,7 +17,6 @@ const Sponsors = () => {
           <ImageDiv name='/spRen.png' url='https://renproject.io/' />
           <ImageDiv name='/spVicara.png' url='https://vicara.co/' />
           <ImageDiv name='/spDeepnote.png' url='https://deepnote.com/' />
-          <ImageDiv name='/spThedapplist.svg' url='https://thedapplist.com/' />
           <ImageDiv name='/spKumospace.png' url='https://www.kumospace.com/' />
           <ImageDiv
             name='/spDigitalocean.svg'
@@ -28,20 +27,6 @@ const Sponsors = () => {
             url='https://www.kaspersky.co.in/'
           />
           <ImageDiv name='/spReplit.png' url='https://replit.com/' />
-          <ImageDiv name='/spGFG.png' url='https://www.geeksforgeeks.org/' />
-          <ImageDiv name='/spEcho.png' url='https://www.echo3d.co/' />
-          <ImageDiv name='/spVoiceflow.png' url='https://www.voiceflow.com/' />
-          <ImageDiv name='/spHashicorp.svg' url='https://www.hashicorp.com/' />
-          <ImageDiv name='/spTaskade.png' url='https://www.taskade.com/' />
-          <ImageDiv name='/sp1password.svg' url='https://1password.com/' />
-          <ImageDiv name='/spFramer.svg' url='https://www.framer.com/' />
-          <ImageDiv name='/spGenxyz.png' url='https://gen.xyz/' />
-          <ImageDiv name='/spEgghead.svg' url='https://egghead.io/' />
-          <ImageDiv
-            name='/spHackthebox.png'
-            url='https://www.hackthebox.com/'
-          />
-          <ImageDiv name='/spSketch.svg' url='https://www.sketch.com/' />
         </div>
       </div>
     </div>
