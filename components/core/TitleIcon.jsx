db9d292f42dfc90a3adb69929f62d05389a80c39
chirantan2003 <chirantan2003@gmail.com>
2022-08-27 17:01:01 +0530

    chore: linting

--- components/core/TitleIcon.jsx
+++ components/core/TitleIcon.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const TitleIcon = (props) => {
   return (
-    <div className='titleIcon'>
+    <div className='titleicon'>
       <Image src={props.img} height='50' width='50' alt='speaker' />
       <h1>{props.title}</h1>
     </div>
