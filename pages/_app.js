1122c2124a047cf9fa4187a3cc81976c1bf8cfb9
chirantan2003 <chirantan2003@gmail.com>
2022-08-27 11:33:35 +0530

    feat: title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <title>DevJams'22</title>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
