c8a9f4267585260ac73cb5a30350c271f455aa31
chirantan2003 <chirantan2003@gmail.com>
2022-09-03 17:06:11 +0530

    feat: add meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
-  return (
-    <>
-      <title>DevJams'22</title>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
