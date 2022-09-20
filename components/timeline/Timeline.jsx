2cdd1ff9376cd8a7d8a4e41b5e413a1db328bb76
chirantan2003 <chirantan2003@gmail.com>
2022-09-20 22:55:54 +0530

    feat: timeline responsive

--- components/timeline/Timeline.jsx
+++ components/timeline/Timeline.jsx
@@ -1,12 +1,18 @@
 import styles from './Timeline.module.css'
 import TitleIcon from '../core/TitleIcon'
+import Image from 'next/image'
 
 const Timeline = () => {
   return (
     <div className='lightbg' id='timeline'>
       <TitleIcon title='Timeline' img='/timeline.svg' />
       <div className={styles.outer}>
-        <h2>Coming Soon!</h2>
+        <Image
+          src='/timelineImg.svg'
+          layout='fill'
+          objectFit='contain'
+          alt='Timeline'
+        />
       </div>
     </div>
   )
