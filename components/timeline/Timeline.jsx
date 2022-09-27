f257787ea3e7a909d8b7f69bd7a130040978fbcf
chirantan2003 <chirantan2003@gmail.com>
2022-09-27 20:33:30 +0530

    feat: update timeline

--- components/timeline/Timeline.jsx
+++ components/timeline/Timeline.jsx
@@ -1,18 +1,39 @@
 import styles from './Timeline.module.css'
 import TitleIcon from '../core/TitleIcon'
+import { Carousel } from '@mantine/carousel'
 import Image from 'next/image'
 
 const Timeline = () => {
   return (
     <div className='lightbg' id='timeline'>
       <TitleIcon title='Timeline' img='/timeline.svg' />
-      <div className={styles.outer}>
-        <Image
-          src='/timelineImg.svg'
-          layout='fill'
-          objectFit='contain'
-          alt='Timeline'
-        />
+      <div className={styles.outer1}>
+        <Carousel
+          height='100%'
+          sx={{ flex: 1 }}
+          controlSize={55}
+          mx='auto'
+          slideGap='md'
+          align='start'
+          styles={{
+            control: {
+              '&[data-inactive]': {
+                opacity: 0,
+                cursor: 'default'
+              }
+            }
+          }}
+        >
+          <Carousel.Slide className={styles.box}>
+            <Image src='timeline1.svg' layout='fill' />
+          </Carousel.Slide>
+          <Carousel.Slide className={styles.box}>
+            <Image src='timeline2.svg' layout='fill' />
+          </Carousel.Slide>
+          <Carousel.Slide className={styles.box}>
+            <Image src='timeline3.svg' layout='fill' />
+          </Carousel.Slide>
+        </Carousel>
       </div>
     </div>
   )
